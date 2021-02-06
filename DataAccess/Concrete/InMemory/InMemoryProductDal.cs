1880d451fe747edca13ae7f2d95839bd77556e0d
aykutozturk27 <aykut.ozturk@hotmail.com>
2021-02-06 11:29:49 +0300

    Ders 8

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -1,7 +1,9 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace DataAccess.Concrete.InMemory
 {
@@ -11,7 +13,7 @@ namespace DataAccess.Concrete.InMemory
         public InMemoryProductDal()
         {
             _products = new List<Product> { 
-                new Product{ProductId=1, CategoryId=2,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
+                new Product{ProductId=1, CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
                 new Product{ProductId=2, CategoryId=2,ProductName="Kamera",UnitPrice=500,UnitsInStock=3},
                 new Product{ProductId=3, CategoryId=2,ProductName="Telefon",UnitPrice=1500,UnitsInStock=2},
                 new Product{ProductId=4, CategoryId=2,ProductName="Klavye",UnitPrice=150,UnitsInStock=65},
@@ -31,9 +33,19 @@ namespace DataAccess.Concrete.InMemory
             _products.Remove(productToDelete);
         }
 
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Product> GetAll()
         {
             return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
         }
 
         public List<Product> GetAllByCategory(int categoryId)
