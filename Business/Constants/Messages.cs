3747cf8c1f5aa95471914583b76cd6c49cec606a
aykutozturk27 <aykut.ozturk@hotmail.com>
2021-02-11 00:04:16 +0300

    10 Şubat Ders Sonu

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,11 @@
+﻿namespace Business.Constants
+{
+    public static class Messages
+    {
+        //public değişkenlerin ilk harfi büyük yazılır.
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductsListed = "Ürünler listelendi";
+    }
+}
